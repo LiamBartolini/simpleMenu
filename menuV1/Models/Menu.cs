1354e7f03622db6298cf8dcbafad754fedfc45d0
LiamBartolini <liambartolini@gmail.com>
2021-03-14 06:04:27 +0100

    first commit

---
+++ menuV1/Models/Menu.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace menuV1.Models
+{
+    static class Menu
+    {
+        static int _selectedIndex;
+        static string[] _options;
+        static string _prompt;
+
+        //a method seems like a constructor
+        static public void Initialize(string prompt, string[] options)
+        {
+            _prompt = prompt;
+            _options = options;
+            _selectedIndex = 0;
+        }
+
+        static private void DisplayOptions()
+        {
+            Console.WriteLine(_prompt);
+            for (int i = 0; i < _options.Length; i++)
+            {
+                string currentOption = _options[i];
+                string prefix;
+                if (i == _selectedIndex)
+                {
+                    prefix = "*";
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    prefix = " ";
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+                Console.WriteLine($"{prefix} << {currentOption} >>");
+            }
+            Console.ResetColor();
+        }
+
+        static public int Run()
+        {
+            ConsoleKey keyPressed;
+            do
+            {
+                Console.Clear();
+                DisplayOptions();
+
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                keyPressed = keyInfo.Key;
+
+                // aggiorno il selected index in base alla freccia usata
+                if (keyPressed == ConsoleKey.UpArrow)
+                {
+                    _selectedIndex--;
+                    if (_selectedIndex == -1)
+                        _selectedIndex = _options.Length - 1;
+                }
+                else if (keyPressed == ConsoleKey.DownArrow)
+                {
+                    _selectedIndex++;
+                    if (_selectedIndex == _options.Length)
+                        _selectedIndex = 0;
+                }
+            } while (keyPressed != ConsoleKey.Enter);
+            return _selectedIndex;
+        }
+    }
+}
---
+++ menuV1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using menuV1.Models;
+
+namespace menuV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string prompt = "Benvenuto!!";
+            string[] options = { "Primo", "Secondo", "Terzo" };
+            Menu.Initialize(prompt, options);
+            int selectedIndex = Menu.Run();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            switch (selectedIndex)
+            {
+                case 0:
+                    Console.WriteLine("PRIMO");
+                    break;
+                case 1:
+                    Console.WriteLine("SECONDO");
+                    break;
+                case 2:
+                    Console.WriteLine("TERZO");
+                    break;
+            }
+
+            Console.ResetColor();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
